dfed1f7296558270e2035e994fa69b3db35c5cfe
An Doan <dtan42@gmail.com>
2020-10-03 13:09:45 +1000

    finish login with access token

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './App.css';
+import Login from './Login';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from './Player';
 
+const spotify = new SpotifyWebApi();
 function App() {
+  const [token, setToken] = useState(null);
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token)
+      spotify.setAccessToken(_token);
+      spotify.getMe().then(user => {
+        console.log(user);
+      })
+    }
+
+  }, [])
   return (
     <div className="app">
-      <h1>An Doan</h1>
+      {token ? (
+        <Player />
+      ) : (
+          <Login />
+        )}
+
     </div>
   );
 }
